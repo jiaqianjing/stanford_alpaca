0330e7d3ab7eb34b3ba64d30ec7aa87a44dae56a
Xuechen Li <lxuechen@gmail.com>
2023-03-16 09:18:09 -0700

    remove failing eval.

---
+++ run.sh
@@ -0,0 +1,20 @@
+torchrun --nproc_per_node=1  train.py \
+    --model_name_or_path  ../llama/hf_weight/llama-7b \
+    --data_path ./alpaca_data.json \
+    --bf16 False \
+    --output_dir ./alpaca-7b-zh \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer'
